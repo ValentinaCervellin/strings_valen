1f10b6dd19ffe85b7eb181156ae54dc4a4f3d746
ValentinaCervellin <valentina.garibotti@escola.pr.gov.br>
2026-05-28 11:44:40 +0000

    tarefa4

--- script.js
+++
@@ -1,7 +0,0 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
-
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
-
-function mostraPalavrasChave() {
-    const texto = document.querySelector('#entrada-de-texto').value
-}
